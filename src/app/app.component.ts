47c02f1936e4e72b7b407299b1b577c11bce8c9d
SamyaAbouKhamis <rouaba134@gmail.com>
2024-05-14 14:38:22 +0300

    create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
-
+import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { CountsComponent } from './counts/counts.component';
+import { ClientsComponent } from './clients/clients.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,HeroComponent],
+  imports: [RouterOutlet,HeaderComponent,HeroComponent,NgbModule,ServicesComponent,AboutComponent,CountsComponent
+  ,ClientsComponent,FeaturedComponent,PortfolioComponent,ContactComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,25 @@ import { Routes } from '@angular/router';
 import { TopbarComponent } from './topbar/topbar.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { CountsComponent } from './counts/counts.component';
+import { ClientsComponent } from './clients/clients.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
     {'path':'', component:TopbarComponent},
     {'path':'header', component:HeaderComponent},
     {'path':'hero', component:HeroComponent},
-   
+    {'path':'featured', component:FeaturedComponent},
+    {'path':'About', component:AboutComponent},
+    {'path':'counts', component:CountsComponent},
+    {'path':'clients', component:ClientsComponent},
+    {'path':'services', component:ServicesComponent},
+    {'path':'portfolio', component:PortfolioComponent},
+    {'path':'contact', component:ContactComponent},
+    {'path':'footer', component:FooterComponent},
 ];
