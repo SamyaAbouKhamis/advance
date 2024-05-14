427b26ca1ccb91575f4311256c94ee72fae11dcf
SamyaAbouKhamis <rouaba134@gmail.com>
2024-05-14 11:49:27 +0300

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TopbarComponent } from './topbar/topbar.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'', component:TopbarComponent},
+    {'path':'header', component:HeaderComponent},
+    {'path':'hero', component:HeroComponent},
+   
+];
