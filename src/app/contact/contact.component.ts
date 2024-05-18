eaba713d58cc3718e0f16a16aadb92e5ecd8f81a
SamyaAbouKhamis <rouaba134@gmail.com>
2024-05-18 15:09:19 +0300

    add google map

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
 import { GoogleMap, GoogleMapsModule } from '@angular/google-maps';
 import { faEnvelope, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types';
@@ -18,15 +18,35 @@ export class ContactComponent {
   mapMarkerIcon: IconDefinition = faMapMarkerAlt;
   phoneIcon: IconDefinition = faPhone;
   
-  center: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
-  zoom = 4;
-  display!: google.maps.LatLngLiteral;
+  // center: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
+  // zoom = 4;
+  // display!: google.maps.LatLngLiteral;
 
-  moveMap(event: google.maps.MapMouseEvent) {
-    this.center = (event.latLng?.toJSON() || { lat: 24, lng: 12 });
+  // moveMap(event: google.maps.MapMouseEvent) {
+  //   this.center = (event.latLng?.toJSON() || { lat: 24, lng: 12 });
+  // }
+
+  // move(event: google.maps.MapMouseEvent) {
+  //   this.display = event.latLng?.toJSON() || { lat: 0, lng: 0 };
+  // }
+  map!: google.maps.Map;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 
-  move(event: google.maps.MapMouseEvent) {
-    this.display = event.latLng?.toJSON() || { lat: 0, lng: 0 };
+  initMap(): void {
+    const mapDiv = document.getElementById('map') as HTMLElement;
+    if (mapDiv) {
+      const mapOptions: google.maps.MapOptions = {
+        center: new google.maps.LatLng(37.7749, -122.4194),
+        zoom: 8
+      };
+      this.map = new google.maps.Map(mapDiv, mapOptions);
+    }
   }
 }
