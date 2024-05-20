93731146461729e42343282853f5d9789c758fa1
NoorAlkhouli <nooralkhouli99@gmail.com>
2024-05-20 14:28:25 +0300

    icon tranlate

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebook,faInstagram } from '@fortawesome/free-brands-svg-icons';
@@ -10,12 +10,14 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/common';
 
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [FontAwesomeModule, MatIconModule,MatMenuModule,MatButtonModule,MatProgressSpinnerModule],
+  imports: [FontAwesomeModule, MatIconModule,MatMenuModule,MatButtonModule,MatProgressSpinnerModule,TranslateModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css',
 })
@@ -24,6 +26,45 @@ export class TopbarComponent {
   faInstagram = faInstagram;
   envelopeIcon: IconDefinition = faEnvelope;
   phoneIcon: IconDefinition = faPhone;
- 
+  @Input() showAnimation = true;
+  constructor(
+    private translateService: TranslateService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.translateService.setDefaultLang('en');
+    this.translateService.use('en');
+  }
+
+  changeLanguage(lang: string) {
+    let htmlTag = this.document.getElementsByTagName(
+      'html'
+    )[0] as HTMLHtmlElement;
+    htmlTag.dir = lang === 'ar' ? 'rtl' : 'ltr';
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+    this.changeCssFile(lang);
+  }
+
+  changeCssFile(lang: string) {
+    let headTag = this.document.getElementsByTagName(
+      'head'
+    )[0] as HTMLHeadElement;
+    let existingLink = this.document.getElementById(
+      'langCss'
+    ) as HTMLLinkElement;
+
+    let bundleName = lang === 'ar' ? 'arabicStyle.css' : 'englishStyle.css';
+
+    if (existingLink) {
+      existingLink.href = bundleName;
+    } else {
+      let newLink = this.document.createElement('link');
+      // newLink.rel = 'stylesheet';
+      // newLink.type = 'text/css';
+      // newLink.id = 'langCss';
+      newLink.href = bundleName;
+      headTag.appendChild(newLink);
+    }
+  }
 
 }
